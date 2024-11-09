617372f7b379f3c672a8dcc466f4121607e1f13c
chuatebongtoi <chitrung.tn@gmai.com>
2024-11-09 21:28:13 +0700

    xong trang otp

--- src/pages/OTP/OTPCode.jsx
+++ src/pages/OTP/OTPCode.jsx
@@ -1,29 +1,58 @@
 import React from 'react'
 
 import { Link } from "react-router-dom";
-
+import { BsFillShieldLockFill} from 'react-icons/bs'
+import OtpInput from "otp-input-react"
+import { useState } from 'react';
+import { CgSpinner } from "react-icons/cg"
+ 
 import './OTPCode.scss'
 
 export default function Login() {
     var diachiemail;
+    const [otp, setOtp] = useState("");
+    const [loading, setLoading] = useState(false);
     return (
         <>
-            <h1>Nhập mã OTP</h1>
-            <p>Chúng tôi vừa gửi mã OTP xác minh {diachiemail}. 
-                Email chỉ có hiệu lực trong 10 phút kể từ thời điểm nhận.</p>
-            <h2>Nhập mã OTP</h2>
-            <div className = "nhapmaOTP">
-                <input type = "textbox"></input>
-                <input type = "textbox"></input>
-                <input type = "textbox"></input>
-                <input type = "textbox"></input>
-                <input type = "textbox"></input>
-                <input type = "textbox"></input>
+            <section className="otp-section">
+
+                <div>
+                    <div className = "otp-something">
+                        <h1>Nhập mã OTP</h1>
+                        <p className = "otp-h1">
+                            Chúng tôi vừa gửi mã OTP xác minh “địa chỉ email”. Email chỉ có hiệu lực trong 10 phút kể từ thời điểm nhận.
+                        </p>
+
+                        <>
+                            <div className= "otp-Bs">
+                                <BsFillShieldLockFill />
+                            </div>
+                            <label htmpFor="ph" className="otp-label">
+                                Nhập mã OTP
+                            </label>
+                            <OtpInput 
+                                value={otp}          // Gán trạng thái giá trị OTP
+                                onChange={setOtp}     // Cập nhật trạng thái khi nhập
+                                OTPLength={6}
+                                otpType="number"
+                                disabled={false}
+                                autoFocus
+                                className="otp-input"
+                            ></OtpInput>
+                            <button className = "otp-button">
+                                
+                                <span>Xác nhận</span>
+                            </button>
+                        </>
+                        
+                    </div>
+                    
+                </div>
+                
+            </section>
+            <div class = "OTP-dk">
+                Điều khoản và Điều kiện, Chính sách An toàn và Bảo mật
             </div>
-            <Link to = {'/password'}>
-                <button type="button" className = "nutxacnhan" >Xác nhận</button>
-            </Link>
-            
         </>
     )
 }
