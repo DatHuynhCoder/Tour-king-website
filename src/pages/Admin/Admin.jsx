07c66a86537684f49a8c232ff4cc69569ced87be
le nguyen quynh anh . <huynhanh.170504@gmail.com>
2024-12-16 18:56:22 +0700

    protected route ?

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { Image } from "react-bootstrap";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 import { FaUser, FaHome } from "react-icons/fa";
 import { MdFlight, MdTimer  } from "react-icons/md";
 import { IoTicketSharp } from "react-icons/io5";
-
-
+import Cookies from 'universal-cookie';
+import { jwtDecode } from 'jwt-decode';
 import './Admin.scss'
 
 
 export default function Admin() {
-
+  const cookie = new Cookies()
+  const navigate = useNavigate()
+  useEffect(() => {
+    if(jwtDecode(cookie.get("accessToken")).isadmin === 0) {
+      alert('Bạn không có quyền truy cập trang này')
+      navigate(-1)
+    }
+  }, [])
     return (
       <>
         
@@ -27,7 +34,7 @@ export default function Admin() {
               <li>
                 <Link to={'/admin/dashbroad'} className = "custome-admin-link">
                   <FaHome size={26}/> &nbsp;
-                  <span>Home</span>
+                  <span>Dashboard</span>
                 </Link>
               </li>
               {/* nut nguoi dung */}
@@ -38,7 +45,7 @@ export default function Admin() {
                 </Link>
               </li>
 
-              <li>
+              {/* <li>
                 <Link to={'/admin/chuyenbay'} className = "custome-admin-link">
                   <MdFlight size={26}/> &nbsp;
                   <span>Chuyến bay</span>
@@ -50,7 +57,7 @@ export default function Admin() {
                   <MdTimer size={26}/> &nbsp;
                   <span>Đổi giờ bay</span>
                 </Link>
-              </li>
+              </li> */}
               <li>
                 <Link to={'/admin/hoanve'} className = "custome-admin-link">
                   <IoTicketSharp size={26}/> &nbsp;
